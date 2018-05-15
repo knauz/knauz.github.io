109097a143eefff012ed010e93636452b479c847
Alexander Ried <ried@mytum.de>
2018-05-15 18:32:42 +0200

    bge.user.js: add configuration and two sites
    add multiple fonts, schemes and a small settings dialog to make everything configurable.
    add two additional sites (TODO: check if they need further customisation)

--- bge.user.js
+++ bge.user.js
@@ -1,29 +1,121 @@
 // ==UserScript==
 // @name         BGE readability enhancer
 // @namespace    http://stef.fi/
-// @version      0.1
-// @description  increase readability for published swiss BGE
+// @version      0.3
+// @description  increase readability of some swiss law related websites
 // @author       Alexander Ried, Stephanie Blaettler
 // @match        http://relevancy.bger.ch/php/clir/http/index.php?*
+// @match        https://entscheide.weblaw.ch/cache.php?*
+// @match        https://www.dike.ch/verwaltungsrecht/*
 // @grant        GM_addStyle
+// @grant        GM_getResourceURL
+// @grant        GM_getValue
+// @grant        GM_setValue
 // @require      http://code.jquery.com/jquery-3.3.1.slim.min.js
+// @resource     fonts https://fonts.googleapis.com/css?family=EB+Garamond|Lato|Noto+Serif|Raleway|Roboto|Source+Sans+Pro
 // ==/UserScript==
 
 var style = `
+@import url('${GM_getResourceURL("fonts")}');
 body {
     background: hsla(210, 60%, 16%, 1);
     color: hsla(230, 100%, 100%, 1);
-    width: 40em;
+    max-width: 40em;
     margin: 0px auto;
-    font-family: "HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif; 
-    font-weight: 300;
-    font-size: large;
     line-height: 1.5;
 }
 a { color: hsla(230, 100%, 100%, 1); }
 details[open] { background: darkgrey }
 .big { font-size: large; }
 .bold { font-weight: bold; }
+`;
+
+var settingsCss = `
+  #knauz_settings {
+    display: inline-block;
+    position: fixed;
+    opacity: 0.5;
+    top: 1rem;
+    left: 1rem;
+    border: solid hsl(330, 100%, 50%) 0.3rem;
+    border-radius: 1rem;
+    background: hsl(330, 100%, 50%);
+    max-width: 15em;
+    color: black;
+    overflow: hidden;
+    line-height: initial;
+    font-size: initial;
+  }
+  #knauz_settings:hover { opacity: 1; }
+  #knauz_settings[open] { background: hsl(330, 100%, 90%); opacity: 1; }
+  #knauz_settings summary::-webkit-details-marker { display: none }
+  #knauz_settings summary { outline: none; background: hsl(330, 100%, 50%); margin: -0.15rem; border-radius: 1rem 1rem 0 0; }
+
+  #knauz_settings > summary:before { display: inline-block; content: "⚙️"; font-size: x-large; margin: 0.3rem; }
+  #knauz_settings[open] > summary:after { display: inline-block; content: "Einstellungen"; }
+
+  #knauz_settings section { padding-left: 0.5em; }
+
+  #knauz_settings h2 { font-size: initial; margin: 1em 0 0 0; }
+
+  #knauz_settings ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+
+  #knauz_settings ul > li {
+    display: inline-block;
+    margin: 0.5rem;
+  }
+
+  #knauz_settings .icon {
+    font-size: x-large;
+    font-family: MONOSPACE;
+  }
+
+  #knauz_settings .btn {
+    cursor: pointer;
+    user-select: none;
+    padding: 0.5rem;
+    border: solid black 1px;
+    border-radius: 1rem;
+  }
+
+`;
+
+var settings = `
+<details id="knauz_settings">
+  <summary />
+  <section>
+    <h2>Farbe</h2>
+    <ul id="knauz_scheme">
+      <li class="btn" style="color: rgb(176, 176, 176); background: rgb(18, 18, 18);">Aa</li>
+      <li class="btn" style="color: rgb(205, 205, 205); background: rgb(90, 90, 90);">Aa</li>
+      <li class="btn" style="color: rgb(79, 50, 28); background: rgb(248, 241, 227);">Aa</li>
+      <li class="btn" style="color: rgb(0, 0, 0); background: rgb(255, 255, 255);">Aa</li>
+    </ul>
+  </section>
+  <section>
+    <h2>Schriftart</h2>
+    <ul id="knauz_font">
+      <li class="btn" style="font-family: 'EB Garamond', serif;">Garamond</li>
+      <li class="btn" style="font-family: 'Helvetica', sans-serif;">Helvetica</li>
+      <li class="btn" style="font-family: 'Lato', sans-serif;">Lato</li>
+      <li class="btn" style="font-family: 'Noto Serif', serif;">Noto Serif</li>
+      <li class="btn" style="font-family: 'Raleway', sans-serif;">Raleway</li>
+      <li class="btn" style="font-family: 'Roboto', sans-serif;">Roboto</li>
+      <li class="btn" style="font-family: 'Source Sans Pro', sans-serif;">Source Sans Pro</li>
+    </ul>
+  </section>
+  <section>
+    <h2>Größe</h2>
+    <ul id="knauz_size">
+      <li class="icon btn" data-multiplier="0.8">&ndash;</li>
+      <li class="icon btn" data-multiplier="1.2">+</li>
+    </ul>
+  </section>
+</details>
 `;
 
 function getSummary(text, length=0) {
@@ -40,18 +132,62 @@ function replacement(match, text) {
     return match;
 }
 
+function set_scheme() {
+    $("body").css("color", this.style.color);
+    $("body").css("background", this.style.background);
+    this.style.borderColor = "blue";
+    $(this).siblings("li").each((i, li) => { li.style.borderColor = "unset"; });
+    GM_setValue("knauz_scheme", $(this).parent().children("li").index(this));
+}
+
+function set_font() {
+    $("body").css("font-family", this.style.fontFamily);
+    this.style.borderColor = "blue";
+    $(this).siblings("li").each((i, li) => { li.style.borderColor = "unset"; });
+    GM_setValue("knauz_font", $(this).parent().children("li").index(this));
+}
+
+function set_size() {
+    var fontSize = parseInt($("body").css("font-size")) || 16;
+    fontSize = fontSize * $(this).data("multiplier");
+    $("body").css("font-size", fontSize + "px");
+    GM_setValue("knauz_size", fontSize);
+}
+
 (function() {
     'use strict';
 
+    // bger.ch
     $("link").remove();
     $("a.noprint").remove();
     $("#ns4_info").remove();
     $(".pagebreak").remove();
-
-    var paras = $(".paraatf");
-    paras.each((i, para) => {
+    $(".paraatf").each((i, para) => {
         para.innerHTML = para.innerHTML.replace(/\(([^(]+?)(\(.+?\))*?\)/g, replacement);
     });
 
+    // custom simple css
     GM_addStyle(style);
+
+    // settings
+    GM_addStyle(settingsCss);
+    $(settings).appendTo("body");
+    $("#knauz_scheme > li").click(set_scheme);
+    $("#knauz_font > li").click(set_font);
+    $("#knauz_size > li").click(set_size);
+
+    // restore saved settings
+    var schemes = $("#knauz_scheme > li");
+    var scheme = Math.max(0, Math.min(schemes.length - 1, GM_getValue("knauz_scheme", 0)));
+    schemes.eq(scheme).trigger("click");
+
+    var fonts = $("#knauz_font > li");
+    var font = Math.max(0, Math.min(fonts.length - 1, GM_getValue("knauz_font", 0)));
+    fonts.eq(font).trigger("click");
+
+    var size = GM_getValue("knauz_size", 16);
+    $("body").css("font-size", size + "px");
+
+
+
 })();
